81be611f69596ed7f560ee96da869dd9d2a11494
rob <mylerch77@gmail.com>
2023-05-11 13:35:31 +0200

    add graphql hotchocolate webapi

---
+++ BlazorApp/MyWebApi/UsersQuery.cs
@@ -0,0 +1,20 @@
+﻿namespace MyWebApi
+{
+    public class UsersQuery
+    {
+        [GraphQLName("users")]
+        [UsePaging(IncludeTotalCount = true)]
+        public IEnumerable<User>  GetAllUsers()
+        {
+            yield return new User()
+            {
+                Name = "rob"
+            };
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+    }
+}
